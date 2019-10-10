0b994094006ebb6114f70da2d5a4280b76fd4f06
Jiří Škoda <skodaji4@fel.cvut.cz>
2019-10-10 08:44:49 +0200

    OMOSet done

--- hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSet.java
+++ hw02/src/main/java/cz/cvut/k36/omo/hw/hw02/OMOSet.java
@@ -15,11 +15,99 @@
  */
 package cz.cvut.k36.omo.hw.hw02;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
+ * Class representing basic class
  * @author Jiri Skoda <skodaji4@fel.cvut.cz>
  */
-public class OMOSet
+public class OMOSet extends OMOSetBase implements OMOSetView
 {
+    private List<Integer> elements;
     
+    /**
+     * Constructor of class
+     * Prepares space for set elements
+     */
+    public OMOSet()
+    {
+        this.elements = new ArrayList<>();
+    }
+    
+    /**
+     * Adds element to the set
+     * @param element Element to be added to set
+     */
+    @Override
+    public void add(int element)
+    {
+        if (this.elements.contains(element) == false)
+        {
+            this.elements.add(element);
+        }
+    }
+    
+    /**
+     * Removes element from the set
+     * @param element Element to be removed from set
+     */
+    @Override
+    public void remove(int element)
+    {
+        if (this.elements.contains(element))
+        {
+            this.elements.remove(element);
+        }
+    }
+    
+    /**
+     * Function to check, whether element is in set or not
+     * @param element Element which should be checked
+     * @return <c>TRUE</c> if element is in the set, <c>FALSE</c> otherwise
+     */
+    @Override
+    public boolean contains(int element)
+    {
+        boolean reti = this.elements.contains(element);
+        return reti;
+    }
+    
+    /**
+     * Gets elements from the set in array
+     * @return Array with elements of the set
+     */
+    @Override
+    public int[] toArray()
+    {
+        int[] reti = new int[this.elements.size()];
+        Iterator<Integer> it = this.elements.iterator();
+        for (int i = 0; i < this.elements.size(); i++)
+        {
+            reti[i] = it.next().intValue();
+        }
+        return reti;
+    }
+    
+    /**
+     * Gets copy of the set
+     * @return Copy of the set
+     */
+    @Override
+    public OMOSetView copy()
+    {
+        OMOSetView reti = null;
+        try
+        {
+            reti = (OMOSetView) this.clone();
+        }
+        catch (CloneNotSupportedException ex)
+        {
+            Logger.getLogger(OMOSet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return reti;
+    }
 }
